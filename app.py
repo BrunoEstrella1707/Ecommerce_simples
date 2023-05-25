4a1c5375d2b39335b02608a05f2d3bb735456689
brunoestrella <brunogamer2002@gmail.com>
2023-05-25 00:46:13 -0300

    second commit

--- app.py
+++ app.py
@@ -3,10 +3,18 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def page_home():
     return render_template("home.html")
 
+@app.route('/produtos')
+def page_produtos():
 
-@app.route('/sobre<usuario>')
-def sobre(usuario):
-    return f"<h3>Sobre mim: {usuario}</h3>"
+    itens = [
+        {'id': 1, 'nome': 'Celular', 'cod': '23423424', 'preco': 1200},
+        {'id': 2, 'nome': 'Notebook', 'cod': '12345678', 'preco': 3500},
+        {'id': 3, 'nome': 'Teclado', 'cod': '54239918', 'preco': 120},
+        {'id': 4, 'nome': 'Monitor', 'cod': '61129763', 'preco': 800}
+    ]
+
+    return render_template("produtos.html", itens=itens)
+
