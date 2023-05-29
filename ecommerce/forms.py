0272e9f642d345cf1ae42a60aee7ba02da7e7e21
brunoestrella <brunogamer2002@gmail.com>
2023-05-29 16:21:48 -0300

    fourth commit

--- ecommerce/forms.py
+++ ecommerce/forms.py
@@ -1,10 +1,35 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
+from ecommerce.models import User
 
 
 class CadastroForm(FlaskForm):
-    usuario = StringField(label='Username: ')
-    email = StringField(label='Email: ')
-    senha1 = PasswordField(label='Senha: ')
-    senha2 = PasswordField(label='Confirme a senha: ')
-    submit = SubmitField(label='Cadastrar')
+    def validate_username(self, check_user):
+        user = User.query.filter_by(usuario=check_user.data).first()
+        if user:
+            raise ValidationError('User alredy registered!')
+
+
+    def validate_email(self, check_email):
+        email = User.query.filter_by(email=check_email.data).first()
+        if email:
+            raise ValidationError('Email alredy registered!')
+        
+
+    def validate_senha(self, check_senha):
+        senha = User.query.filter_by(senha=check_senha.data).first()
+        if senha:
+            raise ValidationError('Password alredy registered!')
+
+
+
+    usuario = StringField(label='Username: ', validators=[Length(min=4, max=30), DataRequired()])
+    email = StringField(label='Email: ', validators=[Email(), DataRequired()])
+    senha1 = PasswordField(label='Password: ',  validators=[Length(min=6, max=15), DataRequired()])
+    senha2 = PasswordField(label='Confirm your Password: ',  validators=[EqualTo('senha1'), DataRequired()])
+    submit = SubmitField(label='Register')
+    
+    
+
+    
